f74badc73b46f6b011d2f484aeea1d20ce193271
Abhi Arya <abhigyaa@uci.edu>
2024-06-10 12:42:21 -0700

    add tests

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/cartographer/YourService.java
@@ -0,0 +1,133 @@
+package jp.jaxa.iss.kibo.rpc.cartographer;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+import gov.nasa.arc.astrobee.Result;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.DetectorParameters;
+import org.opencv.aruco.Dictionary;
+
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.QRCodeDetector;
+
+import android.util.Log;
+
+import org.opencv.core.Mat;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them
+ * in Astrobee.
+ */
+
+public class YourService extends KiboRpcService {
+
+    private final String TAG = "Cartographer";
+
+    @Override
+    protected void runPlan1() {
+        Log.i(TAG, "Test runPlan1");
+        // The mission starts.
+        api.startMission();
+
+        // Move to a point.
+        Point point = new Point(10.9d, -9.92284d, 5.195d);
+        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+
+        // Get a camera image.
+        Mat image = api.getMatNavCam();
+
+        /* *********************************************************************** */
+        /* Write your code to recognize type and number of items in the each area! */
+        /* *********************************************************************** */
+
+        // When you recognize items, let’s set the type and number.
+        api.setAreaInfo(1, "item_name", 1);
+
+        /* **************************************************** */
+        /* Let's move to the each area and recognize the items. */
+        /* **************************************************** */
+
+        // When you move to the front of the astronaut, report the rounding completion.
+        api.reportRoundingCompletion();
+
+        /* ********************************************************** */
+        /* Write your code to recognize which item the astronaut has. */
+        /* ********************************************************** */
+
+        // Let's notify the astronaut when you recognize it.
+        api.notifyRecognitionItem();
+
+        /*
+         * *****************************************************************************
+         * **************************
+         */
+        /*
+         * Write your code to move Astrobee to the location of the target item (what the
+         * astronaut is looking for)
+         */
+        /*
+         * *****************************************************************************
+         * **************************
+         */
+
+        // Take a snapshot of the target item.
+        api.takeTargetItemSnapshot();
+        api.reportMissionCompletion();
+        Log.i(TAG, "End Test runPlan1");
+    }
+
+    @Override
+    protected void runPlan2() {
+        // write your plan 2 here.
+    }
+
+    @Override
+    protected void runPlan3() {
+        // write your plan 3 here.
+    }
+
+    // You can add your method.
+    private String yourMethod() {
+        return "your method";
+    }
+}
+
+class MovementSystem {
+    public void moveTo(Point point, Quaternion quaternion) {
+        final int LOOP_MAX = 10;
+
+        Log.i(TAG, "Moving to: " + point.getX() + ", " + point.getY() + ", " + point.getZ());
+        long start = System.currentTimeMillis();
+
+        Result result = api.moveTo(point, quaternion, true);
+
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+        Log.i(TAG, "[0] moveTo finished in : " + elapsedTime / 1000 + " seconds");
+        Log.i(TAG, "[0] hasSucceeded : " + result.hasSucceeded());
+
+        int loopCounter = 1;
+        while (!result.hasSucceeded() && loopCounter <= LOOP_MAX) {
+
+            Log.i(TAG, "[" + loopCounter + "] " + "Calling moveTo function");
+            start = System.currentTimeMillis();
+
+            result = api.moveTo(point, quaternion, true);
+
+            end = System.currentTimeMillis();
+            elapsedTime = end - start;
+            Log.i(TAG, "[" + loopCounter + "] " + "moveTo finished in : " + elapsedTime / 1000 +
+                    " seconds");
+            Log.i(TAG, "[" + loopCounter + "] " + "hasSucceeded : " + result.hasSucceeded());
+
+            loopCounter++;
+        }
+    }
+}
